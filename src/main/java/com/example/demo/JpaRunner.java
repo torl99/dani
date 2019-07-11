a50cfa2cafb1932e9ba39d6eb23bd21cff467460
dani <dani>
2019-07-11 16:14:19 +0900

    criteria (Type Safed)

--- src/main/java/com/example/demo/JpaRunner.java
+++ src/main/java/com/example/demo/JpaRunner.java
@@ -5,6 +5,9 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 import org.hibernate.Session;
 import org.springframework.boot.ApplicationArguments;
@@ -24,9 +27,13 @@ public class JpaRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Post> query = builder.createQuery(Post.class);
+        Root<Post> root = query.from(Post.class);
+        query.select(root);
 
-        TypedQuery<Post> query = entityManager.createQuery("SELECT p FROM Post AS p", Post.class);
-        List<Post> posts = query.getResultList();
+        List<Post> posts = entityManager.createQuery(query).getResultList();
         posts.forEach(System.out::println);
+
     }
 }
