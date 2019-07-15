24d33d6d558935af033b28d1187b84fb39e66742
dani <dani>
2019-07-15 14:39:28 +0900

    PostRepositoryTest (3)

--- src/test/java/com/example/demo/PostRepositoryTest.java
+++ src/test/java/com/example/demo/PostRepositoryTest.java
@@ -8,6 +8,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -35,8 +37,18 @@ public class PostRepositoryTest {
 
         // When
         List<Post> posts = postRepository.findAll();
+
+        // Then
         assertThat(posts.size()).isEqualTo(1);
         assertThat(posts).contains(newPost);
+
+
+        // When
+        Page<Post> page = postRepository.findAll(PageRequest.of(0, 10));
+        assertThat(page.getTotalElements()).isEqualTo(1);
+        assertThat(page.getNumber()).isEqualTo(0);
+        assertThat(page.getSize()).isEqualTo(10);
+        assertThat(page.getNumberOfElements()).isEqualTo(1);
     }
     
 }
