e4953c64d894ba03845e102d887204fe0a774904
dani <dani>
2019-07-15 14:21:02 +0900

    postRepositoryTest (1)

--- src/main/java/com/example/demo/PostRepository.java
+++ src/main/java/com/example/demo/PostRepository.java
@@ -1,11 +1,7 @@
 package com.example.demo;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    Page<Post> findbyTitleContains(String title, Pageable pageable);
-    
 }
--- src/test/java/com/example/demo/PostRepositoryTest.java
+++ src/test/java/com/example/demo/PostRepositoryTest.java
@@ -2,14 +2,10 @@ package com.example.demo;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.List;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -32,29 +28,6 @@ public class PostRepositoryTest {
 
         // then
         assertThat(newPost.getId()).isNotNull();
-
-        // when
-        List<Post> posts = postRepository.findAll();
-
-        // then
-        assertThat(posts.size()).isEqualTo(1);
-        assertThat(posts).contains(newPost);
-
-        // when
-        Page<Post> page = postRepository.findAll(PageRequest.of(0, 10));
-        assertThat(page.getTotalElements()).isEqualTo(1);
-        assertThat(page.getNumber()).isEqualTo(0);
-        assertThat(page.getSize()).isEqualTo(10);
-        assertThat(page.getNumberOfElements()).isEqualTo(1);
-        
-
-        // WHEN
-        page = postRepository.findbyTitleContains("spring", PageRequest.of(0, 10));
-        assertThat(page.getTotalElements()).isEqualTo(1);
-        assertThat(page.getNumber()).isEqualTo(0);
-        assertThat(page.getSize()).isEqualTo(10);
-        assertThat(page.getNumberOfElements()).isEqualTo(1);
-
     }
     
 }
