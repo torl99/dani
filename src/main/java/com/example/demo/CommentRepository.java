f4a4f1f937176dae52a13cd596f94c998be6ded2
dani <dani>
2019-07-16 11:02:32 +0900

    MyRepository

--- src/main/java/com/example/demo/CommentRepository.java
+++ src/main/java/com/example/demo/CommentRepository.java
@@ -1,14 +1,5 @@
 package com.example.demo;
 
-import java.util.List;
-
-import org.springframework.data.repository.RepositoryDefinition;
-
-@RepositoryDefinition(domainClass = Comment.class, idClass = Long.class)
-public interface CommentRepository {
-
-    Comment save(Comment comment);
-    
-    List<Comment> findAll();
+public interface CommentRepository extends MyRepository<Comment, Long> {
     
 }
---
+++ src/main/java/com/example/demo/MyRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.repository.Repository;
+
+@NoRepositoryBean
+public interface MyRepository<T, id extends Serializable> extends Repository<T, id> {
+
+    
+    <E extends T> E save(E entity);
+
+    List<T> findAll();
+}
