0a1a9a9a803ce6ffb5637d39c793af3602c05486
dani <dani>
2019-07-18 11:25:06 +0900

    CommentRepositoryTest MAKE QUERY PRACTICE(4) _ PageRequest

--- src/main/java/com/example/demo/CommentRepository.java
+++ src/main/java/com/example/demo/CommentRepository.java
@@ -1,9 +1,10 @@
 package com.example.demo;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface CommentRepository extends MyRepository<Comment, Long> {
 
 
-    List<Comment> findByCommentContainsIgnoreCaseOrderByLikeCountDesc(String keyword);
+    Page<Comment> findByCommentContainsIgnoreCase(String keyword, Pageable pageable);
 }
--- src/test/java/com/example/demo/CommentRepositoryTest.java
+++ src/test/java/com/example/demo/CommentRepositoryTest.java
@@ -3,12 +3,13 @@ package com.example.demo;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.List;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -24,10 +25,10 @@ public class CommentRepositoryTest {
         this.createComment(100, "spring data JPA");
         this.createComment(55, "hibernate SPRING");
 
-        List<Comment> comments = commentRepository.findByCommentContainsIgnoreCaseOrderByLikeCountDesc("Spring");
-        assertThat(comments.size()).isEqualTo(2);
-        assertThat(comments).first().hasFieldOrPropertyWithValue("likeCount", 100);
-        
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, "LikeCount"));
+        Page<Comment> comments = commentRepository.findByCommentContainsIgnoreCase("Spring", pageRequest);
+        assertThat(comments.getNumberOfElements()).isEqualTo(2);
+        assertThat(comments).first().hasFieldOrPropertyWithValue("LikeCount", 100);
     }
 
     private void createComment(int LikeCount, String comment) {
